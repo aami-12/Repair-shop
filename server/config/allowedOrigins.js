feb1f28c8e8bf2a36619b73d695ef6af6deb47d8
aami-12 <iamaamir253@gmail.com>
2024-02-28 21:20:34 +0530

    changes-1

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://repair-shop-mdda.onrender.com/',
+    'https://repair-shop-mdda.onrender.com',
 ]
 
 module.exports = allowedOrigins
